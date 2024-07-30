7f9096e34b32be207feb201beb0b6e764609d1f3
wakandee <koechallanhi@gmail.com>
2024-07-30 08:21:55 +0300

    Task 1, gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,36 @@
-
 #!/usr/bin/python3
 """Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
-
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    if len(sys.argv) < 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
+    # Get employee info
+    user_url = f"{base_url}/users/{employee_id}"
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Employee not found")
+        sys.exit(1)
+    
+    employee_name = user_response.json().get('name')
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+    # Get TODO list
+    todo_url = f"{base_url}/users/{employee_id}/todos"
+    todo_response = requests.get(todo_url)
+    tasks = todo_response.json()
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+    # Calculate the number of done tasks and total tasks
+    done_tasks = [task for task in tasks if task.get('completed')]
+    total_tasks = len(tasks)
+    done_count = len(done_tasks)
 
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_count, total_tasks))
     for task in done_tasks:
         print("\t {}".format(task.get('title')))

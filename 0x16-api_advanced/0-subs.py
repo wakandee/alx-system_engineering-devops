0fce748735f92725a72898c8aa4d7778b8b366a6
wakandee <koechallanhi@gmail.com>
2024-08-13 17:50:12 +0300

    '0-subs.py'

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my-app'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
